7adefc6d950e08accd1e536a4dc374b2222dad76
Tikava <wavoleniu@gmail.com>
2024-09-23 21:13:33 +0500

    update

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,4 +1,4 @@
-bind = '127.0.0:5000'
+bind = '127.0.0.1:5000'
 workers = 3
 loglevel = 'debug'
 accesslog = '-'
--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 from http import HTTPStatus
 from flask import Flask, Response, request, make_response
+import requests
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
+SERVICE_URL = getenv('SERVICE_URL')
+CERTIFICATE_PATH = getenv('CERTIFICATE_PATH')
+
 @app.post('/<token>')
 def webhook(token) -> Response:
-    # Optionally, process or log the JSON request data
-    # data = request.get_json()
-    print(token)
+    # Print all the request data
+    print(request.json)
     return Response(status=HTTPStatus.OK)
 
-@app.get('/healthcheck')
+@app.post('/setWebhook')
+def set_webhook() -> Response:
+    # Get the token from the request body
+    token = request.json.get('token')
+
+    with open(CERTIFICATE_PATH, 'rb') as file:
+        files = {'certificate': file}
+        data = {'url': f'{SERVICE_URL}/webhook/{token}'}
+        
+        requests.post(f'https://api.telegram.org/bot{token}/setWebhook', files=files, data=data)
+
+
+@app.get('/health')
 def health() -> Response:
     """For the health endpoint, reply with a simple plain text message."""
     response = make_response("The bot is still running fine :)", HTTPStatus.OK)
